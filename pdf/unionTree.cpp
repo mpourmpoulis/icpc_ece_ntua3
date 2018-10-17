c15b0b80e906aae1ed8cdb486c31cce278746330
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2018-10-17 22:02:20 +0300

    Update unionTree.cpp

--- pdf/unionTree.cpp
+++ pdf/unionTree.cpp
@@ -4,7 +4,7 @@ class UnionTree{
 		bool sameRoot(int x,int y);
 		void unite(int x,int y);
 		int  treePathCompression(int x);
-		int  get_parent(int x){
+		int  find(int x){
 			return treePathCompression(x);
 		}
 	private:
